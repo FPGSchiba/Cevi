bf4d1fceccacfd2dccd99d1a6ad6db4d32658329
Jann Erhardt <59923683+FPGSchiba@users.noreply.github.com>
2021-09-18 22:07:20 +0200

    New Version of HeLa21

---
+++ HELA2021/Gruppenwettbewerb/admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - Wettbewerb</title>
+    <link rel="Stylesheet" href="style.css">
+</head>
+<body>
+  <div id="Header">
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a class="Current" href="#">Admin</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</body>
+</html>
--- HELA2021/Gruppenwettbewerb/index.php
+++ HELA2021/Gruppenwettbewerb/index.php
@@ -4,13 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gruppenwettbewerb</title>
+    <link rel="Stylesheet" href="style.css">
 </head>
   <style>
-    #chartdiv {
-      width: 100%;
-      height: 500px;
-    }
+
     
   </style>
     
@@ -29,17 +27,45 @@
 
   <script>
     am4core.ready(function() {
-    
-    // Themes begin
+
     am4core.useTheme(am4themes_animated);
-    // Themes end
     
-    var chart = am4core.create("chartdiv", am4charts.XYChart);
     const obj = JSON.parse('<?php echo $json_str?>');
 
-    var data = [];
     for (const [group, Dataset] of Object.entries(obj)) {
+      var data = [];
       Dataset.forEach(addArrayElementsToData);
+      var chart = am4core.create("chartdiv-" + group, am4charts.XYChart);
+
+      chart.data = data;
+    
+      // Create axes
+      var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.minGridDistance = 60;
+      
+      var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      
+      var series = chart.series.push(new am4charts.LineSeries());
+      series.dataFields.valueY = "value";
+      series.dataFields.dateX = "date";
+      series.strokeWidth = 3;
+
+      var circleBullet = series.bullets.push(new am4charts.CircleBullet());
+      circleBullet.circle.stroke = am4core.color("#fff");
+      circleBullet.circle.strokeWidth = 2;
+
+      var labelBullet = series.bullets.push(new am4charts.LabelBullet());
+      labelBullet.label.text = "{value}";
+      labelBullet.label.dy = -20;
+      
+      series.tooltip.pointerOrientation = "vertical";
+      
+      chart.cursor = new am4charts.XYCursor();
+      chart.cursor.snapToSeries = series;
+      chart.cursor.xAxis = dateAxis;
+      
+      //chart.scrollbarY = new am4core.Scrollbar();
+      chart.scrollbarX = new am4core.Scrollbar();
     }
 
     function addArrayElementsToData(Data){
@@ -47,35 +73,31 @@
         data.push({date:date, value: Data.value});
     }
 
-    chart.data = data;
-    
-    // Create axes
-    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.minGridDistance = 60;
-    
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    
-    // Create series
-    var series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.valueY = "value";
-    series.dataFields.dateX = "date";
-    series.tooltipText = "{value}"
-    
-    series.tooltip.pointerOrientation = "vertical";
-    
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.snapToSeries = series;
-    chart.cursor.xAxis = dateAxis;
-    
-    //chart.scrollbarY = new am4core.Scrollbar();
-    chart.scrollbarX = new am4core.Scrollbar();
-    
-    }); // end am4core.ready()
+    });
   </script>
 <body>
-    <!-- HTML -->
-    <h1>Test Chart</h1>
-    <div id="chartdiv"></div>
-    <h2 id="demo">Test Text</h2>
+  <div id="Header">
+    <nav>
+      <ul>
+        <li>
+          <a class="Current" href="#">Home</a>
+        </li>
+        <li>
+          <a href="admin.php">Admin</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <div id="managediv">
+  <?php
+    $manage = json_decode($json_str, true);
+    foreach($manage as $key => $value) 
+    {
+      echo '<h1>'.$key.'</h1>';
+      // TODO: Loop through all grou names
+      echo '<div id="chartdiv-'.$key.'"></div>';
+    }
+  ?>
+  </div>
 </body>
 </html>
